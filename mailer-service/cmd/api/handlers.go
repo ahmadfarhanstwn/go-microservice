7b0fe5e6ae3ab926269eb4b1d4e0441790a801e5
ahmadfarhanstwn <ahmadfarhanstwn@gmail.com>
2024-05-08 06:53:12 +0700

    routes for mailer service

---
+++ mailer-service/cmd/api/handlers.go
@@ -0,0 +1,40 @@
+package main
+
+import "net/http"
+
+type mailMessage struct {
+	From    string `json:"from"`
+	To      string `json:"to"`
+	Subject string `json:"subject"`
+	Message string `json:"message"`
+}
+
+func (c *Config) SendMail(w http.ResponseWriter, r *http.Request) {
+	var requestPayload mailMessage
+
+	err := c.readJSON(w, r, &requestPayload)
+	if err != nil {
+		c.errorJson(w, err)
+		return
+	}
+
+	msg := Message{
+		From:    requestPayload.From,
+		To:      requestPayload.To,
+		Subject: requestPayload.Subject,
+		Data:    requestPayload.Message,
+	}
+
+	err = c.Mailer.SendSMTPMessage(msg)
+	if err != nil {
+		c.errorJson(w, err)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "sent to " + requestPayload.To,
+	}
+
+	c.writeJSON(w, http.StatusAccepted, payload)
+}
