babd51f55f739cd47a5e51e2944e17845ac6bfb0
ahmadfarhanstwn <ahmadfarhanstwn@gmail.com>
2024-06-29 08:08:31 +0700

    connect to rabbitmq

--- listener-service/main.go
+++ listener-service/main.go
@@ -1,5 +1,49 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
 
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	rabbitConnection, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConnection.Close()
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backoff = 1 * time.Second
+	var connection *amqp.Connection
+	var err error
+
+	for {
+		connection, err = amqp.Dial("amqp://guest:guest@localhost")
+		if err != nil {
+			fmt.Println("Rabbitmq not yet ready...")
+			counts++
+		} else {
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backoff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
+		time.Sleep(backoff)
+		continue
+	}
+
+	return connection, nil
 }
